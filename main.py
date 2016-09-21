e3ef655323ead98818d7ce2a227f13a68e95a95e
KimberlyWrate <Kimwrate@yahoo.com>
2016-09-20 21:33:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class MainPage(webapp2.RequestHandler):
 
         self.write_form(user_text)
 
-        self.redirect("/")
+        #self.redirect("/")
 app = webapp2.WSGIApplication([('/', MainPage)],
                                 debug=True)   
         
